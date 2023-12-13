a137fa38dc5fab7d6e5c8541d6914be9f0d06a6d
raghadhesham10 <145162913+raghadhesham10@users.noreply.github.com>
2023-12-12 21:58:10 -0800

    spongebob moving

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -2,22 +2,44 @@
 // Raghad Ibrahim
 // Date
 
+let sponge = []; // Array to hold sponge animation frames
+let spongeY;
+let bgImage;  //background
 
-let sponge = [];
+// collectables
+let burgers = [];
+let burgerSpeed = 2;
+let score = 0;
+
+function preload() {
+  //spongebob animation
+  for (let i = 0; i < 4; i++) {
+    sponge.push(loadImage("assets2/" + i + ".png"));
+  }
+  //background
+  bgImage = loadImage("assets2/5.png");
+}
+
 function setup() {
   createCanvas(1920, 1080);
   frameRate(10);
-
-}
-
-
-function preload(){
-  for(let i = 0; i< 3; i++){
-    sponge.push(loadImage("assets/" + i + ".png"));
-  }
+  imageMode(CENTER);
+  // defined in setup bc height/2 is unknown before the canvas is created.
+  spongeY = height / 2;
 }
 
 function draw() {
   background(220);
-  image(sponge[frameCount%3],0,0);
+  // display background
+  image(bgImage, width/2, height/2);
+  // display the current frame of the sponge animation
+  image(sponge[frameCount % 4], 1350, spongeY);
+
+  // Move player with arrow keys
+  if (keyIsDown(UP_ARROW) && spongeY > 150) {
+    spongeY -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW) && spongeY < height - 150) {
+    spongeY += 10;
+  }
 }

36807464a17dba1e42882fd792fb61d17cfc3083
raghadhesham10 <145162913+raghadhesham10@users.noreply.github.com>
2023-12-14 23:18:17 -0800

    Added burgers that move across the screen and increase in speed overtime

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -2,44 +2,88 @@
 // Raghad Ibrahim
 // Date
 
-let sponge = []; // Array to hold sponge animation frames
+let sAnimation = []; // Array to hold sponge animation frames
 let spongeY;
-let bgImage;  //background
+let bImage;  //background
+let gravityPoint;
 
 // collectables
 let burgers = [];
-let burgerSpeed = 2;
+let food;
+let burgerSpeed = 10;
 let score = 0;
 
 function preload() {
   //spongebob animation
   for (let i = 0; i < 4; i++) {
-    sponge.push(loadImage("assets2/" + i + ".png"));
+    sAnimation.push(loadImage("assets2/" + i + ".png"));
   }
   //background
-  bgImage = loadImage("assets2/5.png");
+  bImage = loadImage("assets2/5.png");
+  food = loadImage("assets2/4.png");
 }
 
 function setup() {
   createCanvas(1920, 1080);
   frameRate(10);
   imageMode(CENTER);
-  // defined in setup bc height/2 is unknown before the canvas is created.
   spongeY = height / 2;
+  gravityPoint = height / 2;
 }
 
 function draw() {
-  background(220);
-  // display background
-  image(bgImage, width/2, height/2);
-  // display the current frame of the sponge animation
-  image(sponge[frameCount % 4], 1350, spongeY);
+  gravity();
+  movement();
+  //displaying images
+  image(bImage, width/2, height/2);
+  image(sAnimation[frameCount % 4], 1350, spongeY);
 
-  // Move player with arrow keys
+  // Add new burgers at regular intervals
+  if (frameCount % 80 === 0) {
+    burgers.push(new Burger());
+  }
+
+  // Display and update each burger
+  for (let i = burgers.length - 1; i >= 0; i--) {
+    burgers[i].display();
+    burgers[i].update();
+    // If burger (coin) is off the screen, remove it from the array
+    if (burgers[i].offscreen()) {
+      burgers.splice(i, 1);
+    }
+  }
+}
+
+// Move player with arrow keys
+function movement(){
   if (keyIsDown(UP_ARROW) && spongeY > 150) {
     spongeY -= 10;
   }
   if (keyIsDown(DOWN_ARROW) && spongeY < height - 150) {
     spongeY += 10;
+  }
+}
+
+//creates a garvity effect at the center of the tank
+function gravity(){
+  if (!keyIsPressed){
+    if(spongeY > gravityPoint) spongeY-=15;
+    if(spongeY < gravityPoint) spongeY+=15;
+  }
+}
+
+class Burger{
+  constructor(){
+    this.x = -food.width;
+    this.y = random(height - 150, 150);
+  }
+  display(){
+    image(food, this.x, this.y);
+  }
+  update() {
+    this.x += burgerSpeed;
+  }
+  offscreen() {
+  return this.x > width; // Check if the burger is completely offscreen on the right side
   }
 }
---
+++ Final Project2/sketch.js
@@ -0,0 +1,97 @@
+// Final Project
+// Raghad Ibrahim
+// Date
+
+let sAnimation = []; // Array to hold sponge animation frames
+let bImage;  //background
+let sponge;
+// collectables
+let burgers = [];
+let burgerImage;
+let burgerSpeed = 10;
+let burgerInterval = 80;
+let score = 0;
+
+function preload() {
+  //spongebob animation
+  for (let i = 0; i < 4; i++) {
+    sAnimation.push(loadImage("assets2/" + i + ".png"));
+  }
+  //background
+  bImage = loadImage("assets2/5.png");
+  burgerImage = loadImage("assets2/4.png");
+}
+
+function setup() {
+  createCanvas(1920, 1080);
+  frameRate(10);
+  imageMode(CENTER);
+  sponge = new Character();
+}
+
+function draw() {
+  image(bImage, width/2, height/2);
+  sponge.display();
+  sponge.move();
+  sponge.gravity();
+
+  // Add new burgers at regular intervals
+  if (frameCount % burgerInterval === 0) {
+    burgers.push(new Burger());
+  }
+  // Decrease burger generation interval over time
+  if (burgerInterval > 40) {
+    burgerInterval -= 1;
+  }
+
+  // Display and update each burger
+  for (let i = burgers.length - 1; i >= 0; i--) {
+    burgers[i].display();
+    burgers[i].update();
+    // If burger (coin) is off the screen, remove it from the array
+    if (burgers[i].offscreen()) {
+      burgers.splice(i, 1);
+    }
+  }
+}
+
+class Character{
+  constructor(){
+    this.x = 1350;
+    this.y = height / 2;
+    this.g = height / 2;
+  }
+  move(){
+    if (keyIsDown(UP_ARROW) && this.y > 150) {
+      this.y -= 20;
+    }
+    if (keyIsDown(DOWN_ARROW) && this.y < height - 150) {
+      this.y += 20;
+    }
+  }
+  gravity(){
+    if (!keyIsDown(UP_ARROW) && !keyIsDown(DOWN_ARROW)){
+      if(this.y > this.g) this.y-=15;
+      if(this.y < this.g) this.y+=15;
+    }
+  }
+  display(){
+    image(sAnimation[frameCount % 4], 1350, this.y);
+  }
+}
+
+class Burger{
+  constructor(){
+    this.x = -burgerImage.width;
+    this.y = random(height - 150, 150);
+  }
+  display(){
+    image(burgerImage, this.x, this.y);
+  }
+  update() {
+    this.x += burgerSpeed;
+  }
+  offscreen() {
+  return this.x > width; // returns true if offscreen
+  }
+}

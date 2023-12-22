649581ed23760a9475f5724e6624f821599cd87c
raghad1028 <123210478+raghad1028@users.noreply.github.com>
2023-12-22 14:10:29 -0600

    villian now working properly (boss) with different phases

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -1,7 +1,11 @@
 // Final Project
 // Raghad Ibrahim
 // Date
+//write a code if key code is not a number
+// they are still overlapping
+// sponge is declared in set up, can't use it in the functio
 
+let phase = 4;
 let sAnimation = []; // Array to hold sponge animation frames
 let bImage;  //background
 let sponge;
@@ -15,6 +19,14 @@ let obstacleInterval = 200;
 let jamInterval = 80;
 let score = 0;
 let jamScore = 0;
+//boss
+let bossAnimation = [];
+let startBoss = [];
+let endBoss = [];
+let circles = [];
+let bossTime=0;
+let bossFrame = 0;
+let villain;
 
 function preload() {
   //spongebob animation
@@ -27,7 +39,18 @@ function preload() {
   burgerImage = loadImage("assets/4.png");
   jamImage = loadImage("assets/5.png");
   rock = loadImage("assets/rock.png");
-
+  //boss entrance animation
+  for (let i = 0; i < 7; i++) {
+    startBoss.push(loadImage("assets/villian/" + i + ".png"));
+  }
+  //boss end animation
+  for (let i = 6; i >= 0; i--) {
+    endBoss.push(loadImage("assets/villian/" + i + ".png"));
+  }
+  //boss aniation
+  for (let i = 6; i < 12; i++) {
+    bossAnimation.push(loadImage("assets/villian/" + i + ".png"));
+  }
 }
 
 function setup() {
@@ -39,63 +62,75 @@ function draw() {
   imageMode(CENTER);
   image(bImage, width/2, height/2);
   imageMode(CORNER);
-  sponge.display();
-  sponge.move();
-  sponge.gravity();
   textSize(60);
   text(score, 1670, 90);
   text(jamScore, 1670, 213);
+  moveAndDisplaySponge();
 
-  // Add new burgers and obstacles at regular intervals
-  if (frameCount % burgerInterval === 0) {
-    collectibles.push(new Food(burgerImage));
-  }
-  if (frameCount % obstacleInterval === 0) {
-    obstacles.push(new Obstacle(rock));
-  }
-
-  // // Decrease generation interval over time
-  // if (burgerInterval > 40) burgerInterval -= 1;
-  // if(obstacleInterval > 70) obstacleInterval -=1;
-
-  // Add jams rarely
-  if(random(1000)< 1){
-    collectibles.push(new Food(jamImage));
-  }
-
-  for (let i = collectibles.length - 1; i >= 0; i--) {
-    collectibles[i].display();
-    collectibles[i].update();
-    // If collectible is off the screen, remove it from the array
-    if (collectibles[i].offscreen()) {
-      collectibles.splice(i, 1);
-    }
-    //collide
-    if (collideRectRect(sponge.x + 100, sponge.y - 80, 100, 80, collectibles[i].x, collectibles[i].y, 100, 82)){
-      if (collectibles[i].image === burgerImage){
-        score += 1; //increase burger score
+  if (phase === 1){
+    pushingCollectibles();
+    moveAndDisplayCollectibles();
+    if(obstacles.length === 0){
+      image(startBoss[bossFrame], 0, 250);
+      if(frameCount%7===0)bossFrame++;
+      if(bossFrame===7){
+        bossTime = 0;
+        phase = 2;
       }
-      else{
-        jamScore += 1; //increase jam score
-      }
-      collectibles.splice(i, 1);
+    }
+    else moveAndDisplayObstacles();
+  }
+
+  if (phase === 2){
+    pushingCollectibles();
+    moveAndDisplayCollectibles();
+    image(bossAnimation[int(frameCount/10) % 6], 0, 250);
+    moveAndDisplayCircles();
+    if (frameCount % 70 === 0) {
+      circles.push(new MovingCircles());
+    }
+    bossTime++;
+    if (bossTime === 500) {
+      phase = 3;
+      bossFrame = 0; 
     }
   }
 
- // looping through obstacle arrays
-  for (let i = obstacles.length - 1; i >= 0; i--) {
-    obstacles[i].display();
-    obstacles[i].update();
-    if (obstacles[i].offscreen()) {
-      obstacles.splice(i, 1);
+  if (phase === 3){
+    pushingCollectibles();
+    moveAndDisplayCollectibles();
+    if (bossFrame < 7){
+      image(endBoss[bossFrame], 0, 250);
+      moveAndDisplayCircles();
     }
-    if (collideRectRect(sponge.x + 100, sponge.y - 80, 
-      100, 80, obstacles[i].x, obstacles[i].y, 360, 250)){
-      noLoop()
+    if(frameCount%7===0)bossFrame++;
+    if(bossFrame>=7){
+      if (circles.length === 0) phase = 4;
+      else moveAndDisplayCircles();
     }
+  }
+
+  if (phase === 4){
+    // Add new obstacles at regular intervals
+    if (frameCount % obstacleInterval === 0) {
+      obstacles.push(new Obstacle(rock));
+    }
+    pushingCollectibles();
+    // // Decrease generation interval over time
+    // if (burgerInterval > 40) burgerInterval -= 1;
+    // if(obstacleInterval > 70) obstacleInterval -=1;
+    moveAndDisplayCollectibles();
+    moveAndDisplayObstacles();
   }
 }
 
+function keyPressed(){
+  if (keyCode === 49) phase = 1;
+  if (keyCode === 50) phase = 2;
+  if (keyCode === 51) phase = 3;
+  if (keyCode === 52) phase = 4;
+  if (keyCode === 53) phase = 5;
+}
 
 
 class Character{
@@ -150,5 +185,95 @@ class Obstacle extends Food{
         this.y = random(height - 150, 150);
       }
     }
+  }
+}
+
+class MovingCircles{
+  constructor(){
+    this.y = height/2;
+    this.x = 200;
+    this.speed = 5;
+    this.radius = 30;
+    this.time = random(100);
+  }
+  update() {
+    let n = noise(this.time);
+    this.x += this.speed;
+    this.y += map(n, 0, 1, -10, 10);
+    this.time += 0.01;
+  }
+
+  display() {
+    fill(0, 115, 72);
+    noStroke();
+    circle(this.x, this.y, this.radius * 2);
+  }
+
+  isOffScreen() {
+    return this.x > width;
+  }
+}
+
+function moveAndDisplayCircles() {
+  for (let i = circles.length - 1; i >= 0; i--) {
+    circles[i].update();
+    circles[i].display();
+
+    if (circles[i].isOffScreen()) {
+      circles.splice(i, 1);
+    }
+  }
+}
+
+function moveAndDisplaySponge(){
+  sponge.display();
+  sponge.move();
+  sponge.gravity();
+}
+
+function moveAndDisplayObstacles(){
+  sponge = new Character();
+  for (let i = obstacles.length - 1; i >= 0; i--) {
+    obstacles[i].display();
+    obstacles[i].update();
+    if (obstacles[i].offscreen()) {
+      obstacles.splice(i, 1);
+    }
+    if (collideRectRect(sponge.x + 100, sponge.y - 80, 
+      100, 80, obstacles[i].x, obstacles[i].y, 360, 250)){
+      noLoop()
+    }
+  }
+}
+
+function moveAndDisplayCollectibles(){
+  for (let i = collectibles.length - 1; i >= 0; i--) {
+    collectibles[i].display();
+    collectibles[i].update();
+    // If collectible is off the screen, remove it from the array
+    if (collectibles[i].offscreen()) {
+      collectibles.splice(i, 1);
+    }
+    //collide
+    if (collideRectRect(sponge.x + 100, sponge.y - 80, 100, 80, collectibles[i].x, collectibles[i].y, 100, 82)){
+      if (collectibles[i].image === burgerImage){
+        score += 1; //increase burger score
+      }
+      else{
+        jamScore += 1; //increase jam score
+      }
+      collectibles.splice(i, 1);
+    }
+  }
+}
+
+function pushingCollectibles(){
+  // Add new burgers at regular intervals
+  if (frameCount % burgerInterval === 0) {
+    collectibles.push(new Food(burgerImage));
+  }
+  // Add jams rarely
+  if(random(1000)< 1){
+    collectibles.push(new Food(jamImage));
   }
 }
